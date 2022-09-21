c4f460bc091429f79e7097e0ee467afda3d08ffc
Ricardo <ricardo.santom@educa.jcyl.es>
2022-09-21 07:05:29 +0200

    final

--- src/main/java/ies/sauces/aplicacionbanco/AppBanco.java
+++ src/main/java/ies/sauces/aplicacionbanco/AppBanco.java
@@ -3,12 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.sauces.aplicacionbanco;
+package ies.sauces.aplicacionbanco;
 
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
--- src/main/java/ies/sauces/aplicacionbanco/ComparadorSaldo.java
+++ src/main/java/ies/sauces/aplicacionbanco/ComparadorSaldo.java
@@ -1,4 +1,4 @@
-package es.sauces.aplicacionbanco;
+package ies.sauces.aplicacionbanco;
 
 import java.util.Comparator;
 public class ComparadorSaldo implements Comparator<Cuenta>{
